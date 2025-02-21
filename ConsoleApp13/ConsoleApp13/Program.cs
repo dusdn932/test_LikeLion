6893cddcc3dcaef33714db9c501e847cf96bcaed
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 16:40:15 +0900

    2.21_과제

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용
+            Console.WriteLine("■■■■■■■■■■");
+            Console.WriteLine("엔터를 치면 스토리가 시작됩니다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("엔터를 칠때마다 대사가 넘어가면서 진행이 된다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어느 한 마을이 있었다 그이름은 지니어스 마을.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("이 마을은 머리가 좋기로 유명하여 천재들밖에 없었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그런데 갑자기 평화로운 생활을 즐기던 지니어스 마을에 갑자기 어둠이 닥친다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("스투피드 세계에서 찾아온 대마왕 fool!!!!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("대마왕 fool로 인하여 지니어스 마을이 초토화가 되버리고만다.....");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("대마왕 fool : 크하하 이제 모든 세상은 내가 지배한다!!! 바보바보 빔~~");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("대마왕 fool에게 당한 마을 주민들이 하나같이 바보들이 되었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("주민 1 : 헤헤헤 1더하기1은 창문이지롱~");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("주민 2 : 흐흐흫 2 X 8 이 내팔인가? 끌껄낄");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("국왕이 마을을 지키기 위해 기사들을 총동원하여 대마왕을 막으려 하였지만 역부족이였다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("결국 지니어스 마을에서 국왕은 겨우 벗어나서 마을사람들을 되살리기위해 고민을 하던 중");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어떤 한 용사가 나타나 마을을 되찾아 드리겠다고 약속을 한다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("과연 이 용사는 누구이며 마을을 되찾을수 있을까???");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("COMING SOON.....");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("언젠가 출시.....");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
