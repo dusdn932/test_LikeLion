3f62b73d4fffda2736476cc96fe454408d68e0a6
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 10:05:32 +0900

    c#콘솔 파일 생성 및 테스트출력
    .

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //깃허브 출력
+            Console.WriteLine("깃허브 출력");
+        }
+    }
+}
