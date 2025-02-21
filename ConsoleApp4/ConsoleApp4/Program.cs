30e9d6f64d3654dc57bad82c224f578300a98243
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 11:05:53 +0900

    변수를 만들어 값 저장 후 사용

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting; // 문자열 변수를 선언
+            greeting = "Hello, World!"; // 변수에 값을 저장
+            Console.WriteLine(greeting);//출력 Hello, World!
+        }
+    }
+}
