d6e71137b365004d38e739bc8dafb99bc086a9d9
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 10:23:01 +0900

    변수 선언

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age;//정수형 변수 age선언
+            age = 25;//변수 값 지정
+            Console.WriteLine(age);//변수에 저장된 값 출력 25
+        }
+    }
+}
