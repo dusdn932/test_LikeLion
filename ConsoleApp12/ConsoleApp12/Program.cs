d15f75a110b631764355e4c9370917a594109fe1
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 16:01:40 +0900

    오후 강의 3
    1. 문자열 입력 관련 메서드
    2. 형식변환
    3. 미니과제

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자열 입력 관련 메서드
+            /*Console.Write("이름을 입력하세요: ");
+            string userName = Console.ReadLine(); // 사용자로부터 입력 받기
+            Console.WriteLine($"안녕하세요, {userName}님!"); // 입력값 출력*/
+
+            //형식변환
+            /*Console.Write("나이를 입력하세요: ");
+            string input = Console.ReadLine(); // 사용자로부터 입력 받기
+            int age = int.Parse(input); // 문자열을 정수로 변환
+            Console.WriteLine($"현재" + age + "살이군요!");
+            Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!"); // 변환된 값 사용*/
+
+            //미니과제
+         /*   Console.Write("루인스킬 피해값을 입력하세요: ");
+            string a = Console.ReadLine(); // 사용자로부터 입력 받기
+            float a1 = float.Parse(a); // 문자열을 정수로 변환
+            Console.Write("카드 게이지 획득량을 입력하세요: ");
+            string b = Console.ReadLine(); // 사용자로부터 입력 받기
+            float b1 = float.Parse(b); // 문자열을 정수로 변환
+            Console.Write("각성기 피해값을 입력하세요: ");
+            string c = Console.ReadLine(); // 사용자로부터 입력 받기
+            float c1 = float.Parse(c); // 문자열을 정수로 변환
+            Console.Write("최대 마나값을 입력하세요: ");
+            string d = Console.ReadLine(); // 사용자로부터 입력 받기
+            int d1 = int.Parse(d); // 문자열을 정수로 변환
+            Console.Write("전투 중 마나 회복량값을 입력하세요: ");
+            string e = Console.ReadLine(); // 사용자로부터 입력 받기
+            int e1 = int.Parse(e); // 문자열을 정수로 변환
+            Console.Write("비전투 중 마나회복량을 입력하세요: ");
+            string f = Console.ReadLine(); // 사용자로부터 입력 받기
+            int f1 = int.Parse(f); // 문자열을 정수로 변환
+            Console.Write("이동 속도값을 입력하세요: ");
+            string g = Console.ReadLine(); // 사용자로부터 입력 받기
+            float g1 = float.Parse(g); // 문자열을 정수로 변환
+            Console.Write("탈 것 속도값을 입력하세요: ");
+            string h = Console.ReadLine(); // 사용자로부터 입력 받기
+            float h1 = float.Parse(h); // 문자열을 정수로 변환
+            Console.Write("운반 속도값을 입력하세요: ");
+            string i = Console.ReadLine(); // 사용자로부터 입력 받기
+            float i1 = float.Parse(i); // 문자열을 정수로 변환
+            Console.Write("스킬 재사용 대기시간 감소값을 입력하세요: ");
+            string j = Console.ReadLine(); // 사용자로부터 입력 받기
+            float j1 = float.Parse(j); // 문자열을 정수로 변환
+
+            Console.WriteLine($"루인스킬 피해 : " + a1+"%");
+            Console.WriteLine($"카드 게이지 획득량 :" + b1 + "%");
+            Console.WriteLine($"각성기 피해 : " + c1 + "%");
+            Console.WriteLine($"최대 마나 : " + d1);
+            Console.WriteLine($"전투 중 마나 회복량 : " + e1);
+            Console.WriteLine($"비전투 중 마나회복량 : " + f1);
+            Console.WriteLine($"이동 속도 : " + g1 + "%");
+            Console.WriteLine($"탈 것 속도 : " + h1 + "%");
+            Console.WriteLine($"운반 속도 : " + i1 + "%");
+            Console.WriteLine($"스킬 재사용 대기시간 감소 : " + j1 + "%");*/
+
+
+        }
+    }
+}
