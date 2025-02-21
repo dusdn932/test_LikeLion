b7551544ea9b381f2cc72ebcba6be64d059b4cb9
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 11:39:27 +0900

    미니과제2

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int att = 16755, hp = 78103, a = 36, b=1017, c=41, d=611, e=22, f=39;
+
+            Console.WriteLine("기본특성");
+            Console.WriteLine("공격력 : " + att);
+            Console.WriteLine("최대 생명력 : " + hp +"\n");
+            Console.WriteLine("전투특성");
+            Console.WriteLine("치명 : " + a);
+            Console.WriteLine("특화 : " + b);
+            Console.WriteLine("제압 : " + c);
+            Console.WriteLine("신속 : " + d);
+            Console.WriteLine("인내 : " + e);
+            Console.WriteLine("숙련 : " + f);
+
+
+        }
+    }
+}
