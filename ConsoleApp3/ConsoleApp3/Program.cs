f48ed08087bd0d613464fe30c25d6155aad5c151
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 10:42:00 +0900

    미니과제(용사)

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string name = "knight";
+            char grade = 'S';
+
+            Console.WriteLine("체력 = " + hp);
+            Console.WriteLine("공격력 = " + att);
+            Console.WriteLine("이름 = " + name);
+            Console.WriteLine("등급 = " + grade);
+        }
+    }
+}
