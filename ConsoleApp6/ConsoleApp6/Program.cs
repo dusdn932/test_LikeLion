4ac94980d290f60b9046c1a1737732dc272d2943
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 11:22:09 +0900

    형식이 같은 변수 여러 개를 한 번에 선언하기

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20, z = 30; // 정수형 변수 x, y, z를 선언하고 각각 초기화
+            // 출력
+            Console.WriteLine(x); // 출력: 10
+            Console.WriteLine(y); // 출력: 20
+            Console.WriteLine(z); // 출력: 30
+        }
+    }
+}
