586afedbb7bfaf377dc73e8311b5a859bcaeeeeb
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-21 15:04:03 +0900

    미니과제(로딩바 재현)

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000);//1000당 1초
+            Console.Clear();//콘솔 내용
+
+        }
+    }
+}
